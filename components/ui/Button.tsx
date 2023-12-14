3f7ceb2adab52ed457d991f73a2b9682e1205f3d
Daniel <danielukoha101@gmail.com>
2023-12-14 15:17:32 +0100

    style: add left icon to button component

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,13 +1,15 @@
 type Prop = {
   children: React.ReactNode;
   className?: string;
+  leftIcon?: React.ReactNode;
 };
 
-const Button = ({ children, className }: Prop) => {
+const Button = ({ children, className, leftIcon }: Prop) => {
   return (
     <button
-      className={`py-4 px-6 text-white text-sm font-head font-semibold rounded-md bg-accent ${className}`}
+      className={`py-4 px-6 flex gap-2 text-white text-sm font-head font-semibold rounded-md bg-accent ${className}`}
     >
+      <span>{leftIcon}</span>
       {children}
     </button>
   );
