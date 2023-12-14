830a05d4036bf097fe3fa50162325a60d3b376c9
Daniel <danielukoha101@gmail.com>
2023-12-14 22:33:41 +0100

    style: medical team section

--- app/page.tsx
+++ app/page.tsx
@@ -114,6 +114,44 @@ export default function Home() {
           </ul>
         </div>
       </section>
+      <section className="section py-16 flex flex-col gap-4 items-center lg:gap-24 md:flex-row">
+        <Image
+          src="/assets/images/medical-team.png"
+          alt="medical team"
+          width={400}
+          height={400}
+          className="w-full md:w-1/2"
+        />
+        <div className="flex flex-col gap-4 items-start">
+          <h3 className="font-semibold text-3xl">
+            Our Medical Team works effortlessly
+          </h3>
+
+          <p className="font-medium">
+            Effortlessly manage your appointments with our intuitive doctor's
+            appointment system. Say goodbye to scheduling hassles and hello to
+            seamless, convenient bookings
+          </p>
+          <p className="font-medium">
+            Effortlessly manage your appointments with our intuitive doctor's
+            appointment system. Say goodbye to scheduling hassles and hello to
+            seamless, convenient bookings
+          </p>
+
+          <Button
+            leftIcon={
+              <Image
+                src="/assets/icons/phone.svg"
+                alt="phone icon"
+                width="20"
+                height="20"
+              />
+            }
+          >
+            Contact us
+          </Button>
+        </div>
+      </section>
       <section className="py-16 section font-medium">
         <article className="py-8 flex flex-col gap-4">
           <h3 className="text-3xl font-semibold">Health News</h3>
