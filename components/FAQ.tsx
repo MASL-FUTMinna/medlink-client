1573896b68666011ea36d676476dbfcfcd35a3c2
Daniel <danielukoha101@gmail.com>
2023-12-14 21:48:06 +0100

    feat: create FAQ section and accordion component

---
+++ components/FAQ.tsx
@@ -0,0 +1,48 @@
+import Accordion from "./Accordion";
+import Button from "./ui/Button";
+
+const FAQ = () => {
+  const FAQ_LIST = [
+    {
+      question: "What is this question for?",
+      answer:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ratione! Minus sit illo laboriosam sint tempore quasi vel corrupti? Doloribus dolorem ut debitis ipsa a. Neque accusamus molestias accusantium tempore.",
+    },
+    {
+      question: "What is this question for?",
+      answer:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ratione! Minus sit illo laboriosam sint tempore quasi vel corrupti? Doloribus dolorem ut debitis ipsa a. Neque accusamus molestias accusantium tempore.",
+    },
+    {
+      question: "What is this question for?",
+      answer:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ratione! Minus sit illo laboriosam sint tempore quasi vel corrupti? Doloribus dolorem ut debitis ipsa a. Neque accusamus molestias accusantium tempore.",
+    },
+    {
+      question: "What is this question for?",
+      answer:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ratione! Minus sit illo laboriosam sint tempore quasi vel corrupti? Doloribus dolorem ut debitis ipsa a. Neque accusamus molestias accusantium tempore.",
+    },
+  ];
+  return (
+    <section className="section py-16 flex flex-col gap-8 md:flex-row">
+      <section className="flex flex-col gap-4 items-start">
+        <h3 className="font-semibold text-3xl">Frequently Asked Questions</h3>
+        <p className="mb-4">
+          We are answering most frequent questions. No worries if you not find
+          exact one. You can find out more by searching or continuing clicking
+          button below or directly contact our support.
+        </p>
+
+        <Button>Read all FAQ</Button>
+      </section>
+      <section className="flex flex-col gap-6">
+        {FAQ_LIST.map((faq, index) => (
+          <Accordion key={index} {...faq} />
+        ))}
+      </section>
+    </section>
+  );
+};
+
+export default FAQ;
