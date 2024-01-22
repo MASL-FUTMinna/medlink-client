caa464a6ac9cc919d7a2a354622dec23409a2b51
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2024-01-22 15:36:38 +0100

    feat: update appoinments history

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,32 +1,57 @@
 import Image from "next/image";
 import Link from "next/link";
+import { cookies } from "next/headers";
 
 const Header = () => {
-	return (
-		<header className="sticky top-0 backdrop-blur-xl z-50">
-			<nav className="section w-full flex justify-between items-center">
-				<h2>
-					<Link href="/" className="text-accent-800 font-extrabold text-2xl">
-						Medlink
-					</Link>
-				</h2>
+  const cookieStore = cookies();
+  const token = cookieStore.get("token");
 
-				<div className="flex justify-end gap-20 font-head">
-					<ul className="gap-8 hidden md:flex">
-						<li><Link href="/">Home</Link></li>
-						<li><Link href="/appointments">Appointments</Link></li>
-						<li><a href="/#faq">FAQ</a></li>
-						<li><a href="/#contact">Contact</a></li>
-					</ul>
+  return (
+    <header className="sticky top-0 backdrop-blur-xl z-50">
+      <nav className="section w-full flex justify-between items-center">
+        <h2>
+          <Link href="/" className="text-accent-800 font-extrabold text-2xl">
+            Medlink
+          </Link>
+        </h2>
 
-					<div className="flex gap-2 items-center font-semibold text-accent-800">
-						<Image src="/assets/icons/user.svg" alt="User icon" width={25} height={25} />
-						<Link href="/appointments/sign-in/">Sign in</Link>
-					</div>
-				</div>
-			</nav>
-		</header>
-	);
+        <div className="flex justify-end gap-20 font-head">
+          <ul className="gap-8 hidden md:flex">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/appointments">Appointments</Link>
+            </li>
+            <li>
+              <a href="/#faq">FAQ</a>
+            </li>
+            <li>
+              <a href="/#contact">Contact</a>
+            </li>
+          </ul>
+
+          <div className="flex gap-2 items-center font-semibold text-accent-800">
+            {token ? (
+              <Link href="/appointments/history">Booked Appoiments</Link>
+            ) : (
+              <>
+                <Link href="/appointments/sign-in/">
+                  <Image
+                    src="/assets/icons/user.svg"
+                    alt="User icon"
+                    width={25}
+                    height={25}
+                  />
+                  Sign in
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
