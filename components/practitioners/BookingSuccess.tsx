c7813517a5e96c9d485b598fd77aa2bf2605baa5
Prophet Bestman <bestiproph@gmail.com>
2024-02-02 11:45:30 +0100

    UI FIxes

--- components/practitioners/BookingSuccess.tsx
+++ components/practitioners/BookingSuccess.tsx
@@ -16,7 +16,7 @@ export default function BookingSuccess({
 }: BookingSuccessProps) {
   return (
     <Dialog open={isOpen}>
-      <DialogContent className="bg-white p-8 rounded-md flex justify-center items-center flex-col">
+      <DialogContent className="bg-white w-11/12 md:w-[450px] p-8 px-6 md:px-12 rounded-md flex justify-center items-center flex-col">
         <div className="bg-indigo-500 p-3 rounded-full mb-3">
           <Image
             src="/assets/icons/charm_tick.svg"
@@ -25,14 +25,14 @@ export default function BookingSuccess({
             height={40}
           />
         </div>
-        <p className="text-center text-black text-[32px] font-bold mb-3">
+        <p className="text-center text-black text-xl md:text-[32px] font-bold mb-3">
           Booking Confirmed!
         </p>
-        <p className="mb-4 text-center text-neutral-600 text-[15px] font-normal tracking-tight">
+        <p className="mb-2 text-center text-neutral-600 text-[14px] font-normal tracking-tight">
           The booking confirmation has been sent to your Email Address
         </p>
-        <hr />
-        <div className="mt-8 items-center gap-4 flex justify-center flex-col">
+        <hr className="bg-gray-200 w-full h-0.5" />
+        <div className="mt-2 items-center gap-4 flex justify-center flex-col">
           <p className="text-center text-indigo-500 text-base font-semibold">
             Booking ID - {scheduleResponse.id}
           </p>
@@ -41,7 +41,13 @@ export default function BookingSuccess({
             alt={scheduleResponse.practitioner.first_name}
             width={80}
             height={80}
+            className="rounded-md w-20 h-20 object-cover"
           />
+          <p className="text-xl font-bold">
+            Dr. {scheduleResponse.practitioner.first_name}{" "}
+            {scheduleResponse.practitioner.last_name}
+          </p>
+
           <div className="items-center gap-2 flex justify-center">
             <div className="text-center text-neutral-500 text-sm font-medium flex justify-center items-center gap-2">
               <Image
@@ -53,7 +59,7 @@ export default function BookingSuccess({
               {dateFormat(scheduleResponse.date)}
             </div>
             <div className="rotate-90 border border-neutral-400 w-6"></div>
-            <div className="text-center text-neutral-500 text-sm font-medium flex justify-center items-center gap-2">
+            <div className="text-center text-neutral-500 text-sm font-medium flex justify-center items-center gap-1">
               <Image
                 src="/assets/icons/access-time.svg"
                 alt="time"
